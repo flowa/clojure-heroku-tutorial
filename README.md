590ee4035b9873571884559c09069bbb4cdc773b
pisketti <tero.kadenius@gmail.com>
2014-06-22 02:24:29 +0300

    Updates readme and adds license information to project.clj

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-clojure-heroku-tutorial
-=======================
+# Deploying a Clojure app to Heroku
+
+Source code for the Clojure Heroku tutorial at [http://flowa.fi](http://flowa.fi)
+
+## License
+
+Copyright © 2014 Flowa
+
+The MIT License (MIT)
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject flowa-movie-search "1.0.0-SNAPSHOT"
   :description "Clojure Heroku tutorial source code"
   :url "http://flowa.fi"
-  :license {:name "FIXME: choose"
-            :url "http://example.com/FIXME"}
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
